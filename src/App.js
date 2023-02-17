6e8c640fdf700d9a2f22f17c09e227e0724d6cef
sadat.malik <sadat.malik@me.com>
2023-02-17 21:30:40 +0000

    get data from db backend into FE quiz json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react'
+
 import Title from './components/Title'
 
-
 const App = () => {
+
+    const [quiz, setQuiz] = useState(false)
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://localhost:8000/quiz')
+            const json = response.json()
+            setQuiz(json)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    console.log(quiz)
+
   return (
     <div>
       <Title/>
