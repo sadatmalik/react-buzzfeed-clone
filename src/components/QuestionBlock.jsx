42d1080ca9649d8d871ad7b3bfdc565714ce5bbe
sadat.malik <sadat.malik@me.com>
2023-02-18 23:09:11 +0000

    styling individual question blocks

--- src/components/QuestionBlock.jsx
+++ src/components/QuestionBlock.jsx
@@ -1,5 +1,17 @@
-const QuestionBlock = () => {
-    return <div></div>
+const QuestionBlock = ({ question }) => {
+
+    return (
+        <button
+            className="question-block"
+        >
+            <img src={question.image} alt={question.alt}/>
+            <h3>{question.text}</h3>
+            <p>
+                <a href={question.url}>{question.credit} </a>
+                <a href="https://www.unsplash.com">Unsplash</a>
+            </p>
+        </button>
+    )
 }
 
 export default QuestionBlock
--- src/components/QuestionsBlock.jsx
+++ src/components/QuestionsBlock.jsx
@@ -1,9 +1,18 @@
+import QuestionBlock from './QuestionBlock'
+
 const QuestionsBlock = ({ quizItem }) => {
 
     return (
         <>
             <h2 id={quizItem.id} className="question-title">{quizItem.text}</h2>
-            <div className="questions-container"></div>
+            <div className="questions-container">
+                {quizItem.questions.map((question, _index) => (
+                    <QuestionBlock
+                        key={_index}
+                        question={question}
+                    />
+                ))}
+            </div>
         </>
     )
 }
